e6199dee075e4fba7981ea944a298de3cd1016f4
Pallav Tyagi <pallav.tyagi@outlook.com>
2018-08-02 12:11:25 +0530

    intial setup

---
+++ src/main/java/com/aws/tests/awslearning/AwsLearningApplication.java
@@ -0,0 +1,12 @@
+package com.aws.tests.awslearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsLearningApplication.class, args);
+	}
+}
